943463b740b011abd0c7cd84d020854215b5b29e
Andrew Appleton <aappleton@plaid.com>
2020-04-20 11:22:15 +0100

    Flip test logic

--- tests/flour.ts
+++ tests/flour.ts
@@ -3,25 +3,25 @@ import { Selector } from "testcafe";
 fixture`Wessex Mill: Strong white bread flour`
   .page`https://www.wessexmill.co.uk/acatalog/Strong-White-Bread-Flour-10kg-X049.html`;
 
-test("10 Kg is available", async (t) => {
+test("10 Kg is unavailable", async (t) => {
   await t
     .expect(Selector(".cart-button-placement").innerText)
-    .notContains("Apologies we are offline");
+    .contains("Apologies we are offline");
 });
 
 fixture`Marriages: Strong organic white bread flour`
   .page`https://flour.co.uk/view/strong-organic-white`;
 
-test("1 Kg is available", async (t) => {
+test("1 Kg is unavailable", async (t) => {
   console.log(await Selector(".product-options .row:first-child").innerText);
   await t
     .expect(Selector(".product-options .row:first-child").innerText)
-    .notContains("OUT OF STOCK");
+    .contains("OUT OF STOCK");
 });
 
-test("6x 1 Kg is available", async (t) => {
+test("6x 1 Kg is unavailable", async (t) => {
   console.log(await Selector(".product-options .row:first-child").innerText);
   await t
     .expect(Selector(".product-options .row:nth-child(2)").innerText)
-    .notContains("OUT OF STOCK");
+    .contains("OUT OF STOCK");
 });
